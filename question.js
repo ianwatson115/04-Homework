3728668c16c6f1bd2ee191d54880571038979759
ian <ianwatson115@gmail.com>
2019-12-11 17:21:10 -0500

    created a framework of how I will finish coding the rest of the quiz. created an if and else statements which will eventually be based on what the user clicks on.

--- question.js
+++ question.js
@@ -1,6 +1,9 @@
-var startBtn = document.getElementById("start");
-var timer = 0;
+var startBtn = document.getElementsByClassName("start");
+var divAbove = document.getElementsByClassName("container");
+var timer = document.getElementById("timer");
+var time = 0;
 var score = 0;
+
 
 
 var questions = [
@@ -24,6 +27,33 @@ var questions = [
   ];
 
 function quiz() {
+
+  for (var i = 0; i < questions.length; i++) {
+
+    time = 60;
+    timer.innerHTML(time.value);
+
+    var title = questions[i].title;
+    document.append(title);
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    var choices = document.createElement("button", questions[i].choices);
+    li.appendChild(choices);
+    ul.appendChild(li);
+
+    var userChoice = this.addEventListener(questions.choices)
+
+    
+    var answer = questions[i].answer;
+
+    if (userChoice.innerHTML === questions[i].answer) {
+      document.createElement("");
+      score = currentTime + 1;
+    } else {
+      score = 0;
+    }
+  }
+  
 
 }
   
