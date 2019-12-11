78098f28a93ac4c0f7503947b0a485613c8455c1
ian <ianwatson115@gmail.com>
2019-12-11 16:09:12 -0500

    created some variables and button to start the quiz

--- question.js
+++ question.js
@@ -1,3 +1,8 @@
+var startBtn = document.getElementById("start");
+var timer = 0;
+var score = 0;
+
+
 var questions = [
     {
       title: "Commonly used data types DO NOT include:",
@@ -11,15 +16,16 @@ var questions = [
     },
 
     {
-      title: "",
-      choices: [""],
-      answer: ""
+      title: "Inside the parentheses of a for loop includes the following EXCEPT",
+      choices: ["variable declaration", "update counter" , "condition", "semicolon at the end of the update counter"],
+      answer: "semicolon at the end of the update counter"
     }
     ///etc.
   ];
 
-var startBtn = document.getElementById("start");
-var timer = 0;
+function quiz() {
+
+}
   
   
 
