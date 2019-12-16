67165b0f7110cb8bc0b72b5d505039a3a49287f0
ian <ianwatson115@gmail.com>
2019-12-16 03:20:02 -0500

    finally figured out why my start button wasn't working, needed a DOMContentLoaded function at the start. Now the start button and description hide after button click.

--- quiz.js
+++ quiz.js
@@ -1,54 +1,56 @@
-var startBtn = document.getElementsByClassName("start");
-var paraGrab = document.getElementsByClassName("para");
-var question = document.getElementsByClassName("question");
-var choices = document.getElementsByClassName("choices"); 
-var divAbove = document.getElementsByClassName("container");
-var timer = document.getElementById("timer");
-var time = 0;
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+
+
+var startBtn = document.getElementById("start");
+console.log(startBtn);
+var paraGrab = document.getElementById("para");
+console.log(paraGrab);
+var listDiv = document.getElementById("list-group");  
+var question = document.getElementById("question");
+var choices = document.getElementById("choices"); 
+var container = document.getElementById("container");
+var timeEl = document.getElementById("timer");
+var time;
 var score = 0;
+
+startBtn.addEventListener("click", quizStart);
 
 
 function quizStart() {
     //hide the button and paragraph
-    startBtn.setAttribute("class", "hide");
-    paraGrab.setAttribute("class", "hide");
+  console.log("Wow it started!");
+    startBtn.setAttribute("id", "hide");
+    paraGrab.setAttribute("id", "hide");
 
-    //show the question and choices
-    question.classList.remove("hide");
-    choices.classList.remove("hide");
-
-    quiz();
+    // show the question and choices
+    listDiv.removeAttribute("id");
 }
+// function quiz() {
 
+//   for (var i = 0; i < questions.length; i++) {
 
-function quiz() {
+//     time = 60;
+//     timer.innerHTML(time.value);
 
-  for (var i = 0; i < questions.length; i++) {
+//     var title = questions[i].title;
+//     document.append(title);
+//     var ul = document.getElementById("list");
+//     var li = document.createElement("li");
+//     var choices = document.createElement("button", questions[i].choices);
+//     li.appendChild(choices);
+//     ul.appendChild(li);
 
-    time = 60;
-    timer.innerHTML(time.value);
+//     var userChoice = this.addEventListener(questions.choices);
 
-    var title = questions[i].title;
-    document.append(title);
-    var ul = document.getElementById("list");
-    var li = document.createElement("li");
-    var choices = document.createElement("button", questions[i].choices);
-    li.appendChild(choices);
-    ul.appendChild(li);
+//     var answer = questions[i].answer;
 
-    var userChoice = this.addEventListener(questions.choices)
-
-    var answer = questions[i].answer;
-
-    if (userChoice.innerHTML === questions[i].answer) {
-      document.createElement("");
-      score = currentTime + 1;
-    } else {
-      score = 0;
-    }
-  }
-}
-  
-  
-
-startBtn.addEventListener("click", quizStart());
+//     if (userChoice.innerHTML === questions[i].answer) {
+//       document.createElement("");
+//       score = currentTime + 1;
+//     } else {
+//       score = 0;
+//     }
+//   }
+// }
+});
